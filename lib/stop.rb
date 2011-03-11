7587ec658872b8b94ec9642102bc271bb5e94b7e
Matt Atkins <matt@yoomee.com>
2011-03-11 17:17:20 +0000

    Kill only child proceseseseses

--- lib/stop.rb
+++ lib/stop.rb
@@ -1,9 +1,10 @@
 pid = File.read("node.pid")
 %x{ps -ef | grep #{pid}}.split("\n").each do |line|
-  if !line.match(/grep/)
-    cpid = line.match(/\d+\s(\d+)/)[1]
+  if !line.match(/grep/) && res = line.match(/\d+\s(\d+)\s#{pid}\s/)
+    cpid = res[1]
     %x{kill #{cpid}}
   end
 end
+%x{kill #{pid}}
 %x{rm node.pid}
 puts "Stopped node server."
