b66c0646e66cc365b5b44771122aad28a775653f
Ian Mooney <ian@yoomee.com>
2011-03-15 15:20:00 +0000

    Changes to start and stop scripts so that they save the child pid

--- lib/start.rb
+++ lib/start.rb
@@ -1,9 +1,18 @@
+def get_child_process(ppid)
+  %x{ps -ef | grep #{ppid}}.split("\n").each do |line|
+    if !line.match(/grep/) && res = line.strip.match(/^.*\s+(\d+)\s+#{ppid}\s/)
+      return res[1]
+    end
+  end
+end
+
 pid_file_path = "/var/run/juggernaut.pid"
 if File.exists?(pid_file_path)
   puts "Already runnning with pid #{File.read(pid_file_path)}"
 else
   %x{mkdir -p /var/run}
-  pid = IO.popen("node server.js &> node.log").pid
+  ppid = IO.popen("node server.js &> node.log").pid
+  pid = get_child_process(ppid)
   File.open(pid_file_path, 'w') {|f| f.write(pid)}
   puts "Started node server with pid #{pid}."
 end
--- lib/stop.rb
+++ lib/stop.rb
@@ -1,10 +1,11 @@
 pid_file_path = "/var/run/juggernaut.pid"
 pid = File.read(pid_file_path)
-%x{ps -ef | grep #{pid}}.split("\n").each do |line|
-  if !line.match(/grep/) && res = line.strip.match(/^.*\s+(\d+)\s+#{pid}\s/)
-    cpid = res[1]
-    %x{kill #{cpid}}
-  end
-end
+%x{kill #{pid}}
+# %x{ps -ef | grep #{pid}}.split("\n").each do |line|
+#   if !line.match(/grep/) && res = line.strip.match(/^.*\s+(\d+)\s+#{pid}\s/)
+#     cpid = res[1]
+#     %x{kill #{cpid}}
+#   end
+# end
 %x{rm #{pid_file_path}}
 puts "Stopped node server."
