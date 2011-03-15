10ce2c5772ca564f6619c6a6714dd4df7dc984a3
Matt Atkins <matt@yoomee.com>
2011-03-15 17:31:57 +0000

    Create pid file.

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
+var fs = require("fs");
+
 Juggernaut = require("./index");
 Juggernaut.listen();
+
+// Create pid file
+fs.open("/var/run/juggernaut.pid", "w", 0644, function(err, fd) {
+  if (err) throw err;
+  fs.write(fd, process.pid, 0, "utf8", function(err, written) {
+    if (err) throw err;
+    fs.closeSync(fd);
+  });
+});
+
