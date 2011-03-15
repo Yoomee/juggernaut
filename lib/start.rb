677bced277a090d0d8a7acd5a362ae70bca719c5
Ian Mooney <ian@yoomee.com>
2011-03-15 16:50:47 +0000

    Changes to start and stop

--- lib/start.rb
+++ lib/start.rb
@@ -19,12 +19,11 @@ end
 pid_file_path = "/var/run/juggernaut.pid"
 root_path = File.dirname(__FILE__) + "/../"
 
-if File.exists?(pid_file_path)
-  puts "Already runnning with pid #{File.read(pid_file_path)}."
-elsif pid = get_pid("node server.js")
+if pid = get_pid("node server.js")
   File.open(pid_file_path, 'w') {|f| f.write(pid)}
   puts "Already runnning with pid #{pid}."
 else
+  %x{rm #{pid_file_path}} if File.exists?(pid_file_path)
   %x{mkdir -p /var/run}
   ppid = IO.popen("cd #{root_path} && node server.js &> /var/log/node.log").pid
   if (pid = get_child_process(ppid))
