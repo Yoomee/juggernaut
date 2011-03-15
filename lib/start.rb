3543823c63bc336b1425fc1c41769eabc922cef6
Ian Mooney <ian@yoomee.com>
2011-03-15 10:18:29 +0000

    Changes to start and stop scripts

--- lib/start.rb
+++ lib/start.rb
@@ -1,7 +1,9 @@
-if File.exists?("node.pid")
-  puts "Already runnning with pid #{File.read("node.pid")}"
+pid_file_path = "/var/run/juggernaut/node.pid"
+if File.exists?(pid_file_path)
+  puts "Already runnning with pid #{File.read(pid_file_path)}"
 else
+  %x{mkdir -p /var/run/juggernaut}
   pid = IO.popen("node server.js &> node.log").pid
-  File.open("node.pid", 'w') {|f| f.write(pid)}
+  File.open(pid_file_path, 'w') {|f| f.write(pid)}
   puts "Started node server with pid #{pid}."
 end
--- lib/stop.rb
+++ lib/stop.rb
@@ -1,10 +1,10 @@
-pid = File.read("node.pid")
+pid_file_path = "/var/run/juggernaut/node.pid"
+pid = File.read(pid_file_path)
 %x{ps -ef | grep #{pid}}.split("\n").each do |line|
-  if !line.match(/grep/) && res = line.match(/.*\s(\d+)\s#{pid}\s/)
+  if !line.match(/grep/) && res = line.strip.match(/^.*\s+(\d+)\s+#{pid}\s/)
     cpid = res[1]
     %x{kill #{cpid}}
   end
 end
-%x{kill #{pid}}
-%x{rm node.pid}
+%x{rm #{pid_file_path}}
 puts "Stopped node server."
